c6f143a4e0fa4100675e383b1f930a1c5d49ac28
Jonas Heubuch <jonas.heubuch@outlook.com>
2025-01-28 20:40:28 +0100

    chore: Add Swagger UI

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -17,6 +17,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
 }
 
 app.UseHttpsRedirection();
